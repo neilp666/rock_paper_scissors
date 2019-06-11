722f509b036894728e0eb487da25271dcacc0b57
neil patel <neil@myindianweddingapp.com>
2019-06-11 16:05:26 +0100

    Added random options for computer

--- app.js
+++ app.js
@@ -13,7 +13,25 @@ const game = () => {
         });
     };
 
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.player-computer');
+        
+        const computerOptions = ['rock', 'paper', 'scissors'];
+        const computerNumber = Math.floor(Math.random() * 3);
+        
+        options.forEach(option => {
+            option.addEventListener("click", function () {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                console.log(computerChoice);
+            });
+        });
+    };
+
     startGame();
+    playMatch();
 
 };
 
